9833eb8d7f3bacc3eae39e4c054c07a3bc1c571b
Stephen Brennan <smb196@case.edu>
2016-09-24 15:34:41 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,51 @@
+sitebuilder
+===========
+
+This repository contains a simple Python tool for building all of your GitHub
+pages static sites at once, in a somewhat similar way to how it's done in GitHub
+pages.
+
+It's great if you have a bunch of stuff on GitHub pages, but you'd like to
+migrate to AWS (for SSL + custom domain reasons, perhaps).
+
+Usage
+-----
+
+You need to have Python 3, Ruby+Jekyll+github-pages gem installed.
+
+Then, create a file called `config.py` containing info about your GitHub pages
+repositories.  Mine is given below as an example:
+
+```python
+USERNAME = 'brenns10'
+REPOS = [
+    # (repo, branch, url)
+    ('brenns10.github.io', 'master', ''),
+    ('talks', 'gh-pages', 'talks/'),
+    ('notes', 'gh-pages', 'notes/'),
+    ('libstephen', 'gh-pages', 'libstephen/'),
+    ('cky', 'gh-pages', 'cky/'),
+    ('yams', 'gh-pages', 'yams/'),
+    ('nosj', 'gh-pages', 'nosj/'),
+]
+```
+
+Finally, run `python sitebuilder.py` and the magic should happen. To view your
+site, you could:
+
+```bash
+$ cd site
+$ python -m http.server
+```
+
+And then navigate to http://localhost:8000/
+
+Any time you update a github pages repository, you can update it by simply
+running `python sitebuilder.py` again.
+
+Deployment
+----------
+
+Although this project doesn't do much to assist with deployment, a good next
+step would be to use the `s3_website` gem to deploy your `site` folder to AWS
+and Cloudfront!
---
+++ sitebuilder.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python3
+"""
+Takes all your GitHub pages repos and builds their static sites.
+"""
+
+import os
+import os.path
+import shutil
+from subprocess import check_call, check_output
+
+PATH = 'https://github.com/{username}/{reponame}'
+
+
+class GhpRepo(object):
+    """A class representing a git repository with GitHub pages enabled.
+
+    Has a remote URL, a local directory, and a "build" directory, where the
+    actual generated site will be placed. Also, most repositories are on branch
+    gh-pages, but some (like *.github.io) are on master, so this accepts a
+    branch argument.
+
+    This class allows you to clone, build, update, and rebuild a repo fairly
+    easily.  Of course, it just shells out to git and jekyll.
+
+    """
+
+    def __init__(self, url, local, output, branch='gh-pages'):
+        """Create the class instance.
+
+        This doesn't necessarily clone the repo or build it - in fact, the repo
+        may already exist locally.
+
+        """
+        self.url = url
+        self.local = local
+        self.output = output
+        self.branch = branch
+
+    def checkout(self):
+        """Check out the repository."""
+        check_call(['git', 'clone', '-b', self.branch, self.url, self.local])
+        self.should_build = not os.path.exists(os.path.join(self.local, '.nojekyll'))
+
+    def current_commit(self):
+        """Return the SHA of the current commit."""
+        os.chdir(self.local)
+        return check_output(['git', 'rev-parse', 'HEAD'])
+
+    def pull(self):
+        """Pull from the default remote tracking branch, hopefully."""
+        os.chdir(self.local)
+        check_call(['git', 'pull'])
+        self.should_build = not os.path.exists(os.path.join(self.local, '.nojekyll'))
+
+    def build(self):
+        """Run the build command (jekyll) or just copy files if .nojekyll exists."""
+        os.chdir(self.local)
+        if self.should_build:
+            check_call(['jekyll', 'build', '-d', self.output])
+        else:
+            if os.path.exists(self.output):
+                shutil.rmtree(self.output)
+            shutil.copytree(self.local, self.output)
+
+    def update_and_build(self):
+        """Pull, and if the repo has changed, rebuild."""
+        old_revision = self.current_commit()
+        self.pull()
+        if self.current_commit() != old_revision:
+            self.build()
+
+
+class GhpBuilder(object):
+    """This class represents a collection of GitHub pages repositories to deploy.
+
+    Construct an instance of this object to build your complete GitHub pages
+    website for later deployment.
+
+    """
+
+    def __init__(self, username, repos, directory=os.getcwd()):
+        directory = os.path.abspath(directory)
+        self.username = username
+        self.directory = directory
+        self.work = os.path.join(directory, 'work')
+        self.site = os.path.join(directory, 'site')
+        self.repos = []
+        for name, branch, path in repos:
+            url = PATH.format(username=username, reponame=name)
+            localwork = os.path.join(self.work, path)
+            localbuild = os.path.join(self.site, path)
+            self.repos.append(GhpRepo(url, localwork, localbuild, branch))
+
+    def initial_build(self):
+        """Clone and build for the first time"""
+        os.makedirs(self.work, exist_ok=True)
+        os.makedirs(self.site, exist_ok=True)
+        for repo in self.repos:
+            repo.checkout()
+            repo.build()
+
+    def build(self):
+        """Call initial_build() if this is the first time, otherwise update and build."""
+        if not (os.path.exists(self.work) and os.path.exists(self.site)):
+            self.initial_build()
+            return
+
+        for repo in self.repos:
+            repo.update_and_build()
+
+
+if __name__ == '__main__':
+    from config import USERNAME, REPOS
+    builder = GhpBuilder(USERNAME, REPOS)
+    builder.build()
