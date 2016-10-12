a4a1a4ca5a13f9c3a34c0e42dd3514c512bd72fb
Stephen Brennan <smb196@case.edu>
2016-10-12 15:22:05 -0400

    Add real CLI, address "main" site build issues.

--- sitebuilder.py
+++ sitebuilder.py
@@ -6,9 +6,25 @@ Takes all your GitHub pages repos and builds their static sites.
 import os
 import os.path
 import shutil
-from subprocess import check_call, check_output
+from subprocess import check_call, check_output, DEVNULL
 
 PATH = 'https://github.com/{username}/{reponame}'
+HELPTEXT = """usage: {} [SUBCOMMAND]
+Build and rebuild GitHub pages sites.  Typical usage is to run with no
+arguments, which runs the "dwim" sub-command.
+
+dwim - Automatically guess the operations necessary to completely build your
+       site.  Will tell you what it's doing as it goes. (this is default)
+init - Initialize the site by pulling and running the first build.
+pull - Pull every repository (but do not build anything).
+rebuild - Rebuild the entire site (without pulling first).
+build - Pull each repository and rebuild if it updated.
+help - Display this help text.
+"""
+
+
+def run(args):
+    check_call(args, stdout=DEVNULL, stderr=DEVNULL)
 
 
 class GhpRepo(object):
@@ -24,22 +40,25 @@ class GhpRepo(object):
 
     """
 
-    def __init__(self, url, local, output, branch='gh-pages'):
+    def __init__(self, name, url, local, output, branch='gh-pages'):
         """Create the class instance.
 
         This doesn't necessarily clone the repo or build it - in fact, the repo
         may already exist locally.
 
         """
+        self.name = name
         self.url = url
         self.local = local
         self.output = output
         self.branch = branch
 
+    def should_build(self):
+        return not os.path.exists(os.path.join(self.local, '.nojekyll'))
+
     def checkout(self):
         """Check out the repository."""
-        check_call(['git', 'clone', '-b', self.branch, self.url, self.local])
-        self.should_build = not os.path.exists(os.path.join(self.local, '.nojekyll'))
+        run(['git', 'clone', '-b', self.branch, self.url, self.local])
 
     def current_commit(self):
         """Return the SHA of the current commit."""
@@ -47,17 +66,23 @@ class GhpRepo(object):
         return check_output(['git', 'rev-parse', 'HEAD'])
 
     def pull(self):
-        """Pull from the default remote tracking branch, hopefully."""
+        """Pull from the default remote tracking branch, hopefully.
+
+        Return True if updated, false otherwise.
+        """
         os.chdir(self.local)
-        check_call(['git', 'pull'])
-        self.should_build = not os.path.exists(os.path.join(self.local, '.nojekyll'))
+        old_revision = self.current_commit()
+        run(['git', 'pull'])
+        return self.current_commit() != old_revision
 
     def build(self):
         """Run the build command (jekyll) or just copy files if .nojekyll exists."""
         os.chdir(self.local)
-        if self.should_build:
-            check_call(['jekyll', 'build', '-d', self.output])
+        if self.should_build():
+            print('=> {}: build via Jekyll'.format(self.name))
+            run(['jekyll', 'build', '-d', self.output])
         else:
+            print('=> {}: build via copy'.format(self.name))
             if os.path.exists(self.output):
                 shutil.rmtree(self.output)
             ignore = shutil.ignore_patterns(
@@ -67,12 +92,15 @@ class GhpRepo(object):
                 '.nojekyll'
             )
             shutil.copytree(self.local, self.output, ignore=ignore)
+        print('   DONE'.format(self.name))
 
     def update_and_build(self):
-        """Pull, and if the repo has changed, rebuild."""
-        old_revision = self.current_commit()
-        self.pull()
-        if self.current_commit() != old_revision:
+        """Pull, and if the repo has changed, rebuild.
+
+        Return True if the repo updated.  Return False otherwise.
+        """
+        if self.pull():
+            print('=> {}: was out of date'.format(self.name))
             self.build()
 
 
@@ -91,31 +119,111 @@ class GhpBuilder(object):
         self.work = os.path.join(directory, 'work')
         self.site = os.path.join(directory, 'site')
         self.repos = []
+        self.main = None
         for name, branch, path in repos:
             url = PATH.format(username=username, reponame=name)
             localwork = os.path.join(self.work, name)
             localbuild = os.path.join(self.site, path)
-            self.repos.append(GhpRepo(url, localwork, localbuild, branch))
+            if path == '':  # this is the "main site"
+                self.main = GhpRepo(name, url, localwork, localbuild, branch)
+            else:
+                self.repos.append(GhpRepo(name, url, localwork, localbuild, branch))
 
-    def initial_build(self):
-        """Clone and build for the first time"""
+    def _initial_checkout(self):
+        """Create workdir and clone repos for the first time."""
         os.makedirs(self.work, exist_ok=True)
-        os.makedirs(self.site, exist_ok=True)
+        if self.main:
+            self.main.checkout()
         for repo in self.repos:
             repo.checkout()
+
+    def rebuild(self):
+        """Build all repos, assuming they are already cloned."""
+        os.makedirs(self.site, exist_ok=True)
+        if self.main:
+            self.main.build()
+        for repo in self.repos:
             repo.build()
 
-    def build(self):
-        """Call initial_build() if this is the first time, otherwise update and build."""
-        if not (os.path.exists(self.work) and os.path.exists(self.site)):
-            self.initial_build()
-            return
+    def init(self):
+        """Check out and build for the first time."""
+        self.initial_checkout()
+        self.rebuild()
 
+    def pull(self):
+        """Pull all repositories without updating."""
+        updated = False
+        if self.main and self.main.pull():
+            print('=> {}: updated'.format(self.main.name))
+            updated = True
+        for repo in self.repos:
+            if repo.pull():
+                print('=> {}: updated'.format(repo.name))
+                updated = True
+        if updated:
+            print('You have pulled but not built.  Your site/ directory will')
+            print('only be up-to-date when you run "rebuild".')
+
+    def _update_main(self):
+        """Update the main repository, and possibly subdirectories.
+
+        Since the main site resides in the root, when it is regenerated, the
+        subdirectories are clobbered! To address this, whenever the main repo
+        is rebuilt, all other repos are forced to rebuild.
+
+        """
+        if self.main and self.main.update_and_build():
+            for repo in self.repos:
+                repo.build()
+
+    def build(self):
+        """Normal build."""
+        self._update_main()
         for repo in self.repos:
             repo.update_and_build()
 
+    def dwim(self):
+        """Do what I mean mode!
+
+        This tries to infer what operation should be done based on the state of
+        the directories.  The goal here is always a completely built site.
+
+        """
+        if not os.path.exists(self.work):
+            print('Detected that the work directory does not exist.')
+            print('Initializing ...')
+            self.init()
+        elif not os.path.exists(self.site):
+            print('Detected that the site directory does not exist, but your repos are cloned.')
+            print('Rebuilding ...')
+            self.rebuild()
+        else:
+            print('Updating your existing site.')
+            self.build()
+
+
+def help():
+    print(HELPTEXT.format(sys.argv[0]))
+
 
 if __name__ == '__main__':
+    import sys
     from config import USERNAME, REPOS
     builder = GhpBuilder(USERNAME, REPOS)
-    builder.build()
+    commands = {
+        'init': builder.init,
+        'build': builder.build,
+        'rebuild': builder.rebuild,
+        'dwim': builder.dwim,
+        'pull': builder.pull,
+        'help': help,
+    }
+    if len(sys.argv) <= 1:
+        cmd = 'dwim'
+    else:
+        cmd = sys.argv[1]
+    func = commands.get(cmd)
+    if func:
+        func()
+    else:
+        help()
